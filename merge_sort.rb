4e87ed0f78eae5bc486bb15126c388d7d1e17e29
Alex Hardt <hardt.alex@gmail.com>
2015-02-20 18:31:03 -0600

    lots of comments to see and understand code more ganularly

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,41 +1,17 @@
 # input: unsorted array
 # output: sorted array
 
-
-
-  #for each element of the array,
-  #if the first element is smaller than the next element, do nothing
-  #if the first element is the same, do nothing
-  # if the first element is bigger, swith the two elements
-  # repeat the process with the next two
-
-  #i think there is an edge case here depending on which side you start
-
-  #so base case is:
-
-  # if array.length = 1
-  #   return 'sorted! + #array'
-  # else
-  #   if array[0] > array[1]
-
-  # if first_half[0] == second_half[0]
-    #   output_array << first_half.shift
-    # elsif first_half[0] > second_half[0]
-    #   output_array << second_half.shift
-    # else first_half[0] < second_half[0]
-    #   output_array << first_half.shift
-    # end
-
 def merge_sort(array)
-  p '1. step one, begin method'
+  p "1. step one, begin method array.size is: #{array.size}"
   return array if array.size < 2
-  p '2. just tried to print the array if the size is less than 2'
+  p '2. just tried to return the array if the size is less than 2'
   midpoint = array.size / 2
   p '3. just defined midpoint'
   first_array = merge_sort(array[0...midpoint])
   p '4. just defined first array'
   second_array = merge_sort(array[midpoint..-1])
   p '5. just defined second array'
+  p '6. about to make merge call...'
   merge(first_array, second_array)
 
 end
@@ -48,16 +24,10 @@ def merge(first_array, second_array)
   p second_array
   if first_array[0] < second_array[0]
     [first_array[0]] + merge(first_array[1..-1],second_array)
-
   else
     [second_array[0]] + merge(first_array,second_array[1..-1])
-
   end
 end
-
-
-
-
 
 # p merge_sort([1,2,3,4,5])
 p merge_sort([99,0,5,43,3,0,22,1])
