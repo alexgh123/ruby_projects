6615e68efdc807efefcdb10e704446af3ba786f7
Alex Hardt <hardt.alex@gmail.com>
2015-02-20 18:24:45 -0600

    i have all kinda comments so you can explicitly follow the code after you run it in the command line

--- merge_sort.rb
+++ merge_sort.rb
@@ -27,21 +27,31 @@
     # end
 
 def merge_sort(array)
+  p '1. step one, begin method'
   return array if array.size < 2
+  p '2. just tried to print the array if the size is less than 2'
   midpoint = array.size / 2
+  p '3. just defined midpoint'
   first_array = merge_sort(array[0...midpoint])
+  p '4. just defined first array'
   second_array = merge_sort(array[midpoint..-1])
+  p '5. just defined second array'
   merge(first_array, second_array)
+
 end
 
 def merge(first_array, second_array)
   return second_array if first_array.empty?
   return first_array if second_array.empty?
-
+  p 'as we enter merge land arrays are:'
+  p first_array
+  p second_array
   if first_array[0] < second_array[0]
     [first_array[0]] + merge(first_array[1..-1],second_array)
+
   else
     [second_array[0]] + merge(first_array,second_array[1..-1])
+
   end
 end
 
@@ -50,8 +60,4 @@ end
 
 
 # p merge_sort([1,2,3,4,5])
-p merge_sort([5,43,3,22,1])
-p merge_sort([1,2,1])
-p merge_sort([5,4,5])
-p '-----'
-p merge_sort([5])
+p merge_sort([99,0,5,43,3,0,22,1])
