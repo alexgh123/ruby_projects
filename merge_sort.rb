44ea0afd56e467126577b6eb13ecde86a77df66b
Alex Hardt <hardt.alex@gmail.com>
2015-02-20 18:44:47 -0600

    throwing in the towel

--- merge_sort.rb
+++ merge_sort.rb
@@ -2,26 +2,16 @@
 # output: sorted array
 
 def merge_sort(array)
-  p "1. step one, begin method array.size is: #{array.size}"
   return array if array.size < 2
-  p '2. just tried to return the array if the size is less than 2'
   midpoint = array.size / 2
-  p '3. just defined midpoint'
   first_array = merge_sort(array[0...midpoint])
-  p '4. just defined first array'
   second_array = merge_sort(array[midpoint..-1])
-  p '5. just defined second array'
-  p '6. about to make merge call...'
   merge(first_array, second_array)
-
 end
 
 def merge(first_array, second_array)
   return second_array if first_array.empty?
   return first_array if second_array.empty?
-  p 'as we enter merge land arrays are:'
-  p first_array
-  p second_array
   if first_array[0] < second_array[0]
     [first_array[0]] + merge(first_array[1..-1],second_array)
   else
