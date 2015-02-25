3cf84b7fe45c910393520bacdfb16cec5981e6cc
Alex Hardt <hardt.alex@gmail.com>
2015-02-25 09:17:43 -0600

    made some tests for ceasar

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -17,7 +17,7 @@
 #so i recognize my first attempt at the ceasar_cipher was kinda weak, so now im going to make the good one, but i should also be able to have the tests work
 
 def ceasar_cipher(input, num)
-  input.split(//).map {|x|x.ord}#.map{|x| x + num}.map{|x|x.chr}.join
+  input.split(//).map {|x|x.ord}.map{|x| x + num}.map{|x|x.chr}.join
 end
 
 def easy_to_read_ceasar_cipher(input, shift_number)
@@ -27,7 +27,7 @@ def easy_to_read_ceasar_cipher(input, shift_number)
   scrambled_string = shift_num_array.map {|shift_num| shift_num.chr}.join
 end
 
-# p ceasar_cipher("hello, I'm Alexz", 1)
+# p ceasar_cipher()
 
 # p ceasar_cipher("AZaz", 1)
 
@@ -44,14 +44,16 @@ end
 
 #what is my goal: build a more correct ceasar_cipher
 
-def ceasars_palace(string, num)
-  string.each_byte { |i|
-    case i when (65..90),(97..122) then offset = i + num
-    (offset > 90 && offset < 97 ? offset = offset - 26 : nil) else offset = i end
-    print offset.chr }
-end
+# def ceasars_palace(string, num)
+#   string.each_byte { |i|
+#     case i when (65..90),(97..122) then offset = i + num
+#     (offset > 90 && offset < 97 ? offset = offset - 26 : nil) else offset = i end
+#     print offset.chr }
+# end
 
- p ceasars_palace("AZ",2)
- p ceasars_palace("az",2)
- p "hello"
- p ceasars_palace("hello", 5)
+#  p ceasars_palace("AZ",2)
+#  p ceasars_palace("az",2)
+#  p "hello"
+# ceasars_palace("hello", 5)
+
+# ceasars_palace("HeLLo, I'm AlEx",3)
--- ceasar_cipher_spec.rb
+++ ceasar_cipher_spec.rb
@@ -5,3 +5,17 @@
 #it actually works, aka it actually ciphers stuff
 
 #i wont accept characters with less than 32 chr or 126
+
+require_relative 'ceasar_cipher'
+
+describe '#ceasar_cipher' do
+  scrambled_word = ceasar_cipher("hello, I'm Alexz", 1)
+  cap_word = ceasar_cipher("HeLLo, I'm AlEx",3)
+  it "scrambles the word" do
+    expect(scrambled_word).to eq("ifmmp-!J(n!Bmfy{")
+  end
+
+  it "handles upper and lower case" do
+    expect(cap_word).to eq("KhOOr/#L*p#DoH{")
+  end
+end
