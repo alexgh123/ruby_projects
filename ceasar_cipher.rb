68013fab317e91ee3c5b36e1b657994781fe4cf4
Alex Hardt <hardt.alex@gmail.com>
2015-02-25 07:59:20 -0600

    making better ceasar cipher then testing for it

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,25 +1,23 @@
-  # > caesar_cipher("What a string!", 5)
-  #   => "Bmfy f xywnsl!"
-
-  #   In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.
-
-# input: the word, number of letters to shift
-# output: the word, shifted
-
-# first we will take the input word,
-#we will then turn the word into an array of each individual letter. (2 steps?)
-#for each individaul letter, we will then change it to the number it is represented by on teh ASCII number range thingy,
-#we will then add the number argument passed in.
-#we now have an array of numbers.
-#we will change the new numbers into the +num numbers.
-#next we change the new numbers into new letters.
-#we will then compress the letter array back into words array, then
 
 
-# quick and dirty version:
+# def ceasar_cipher(input, num)
+#   input.split(//).map {|x|x.ord}.map{|x| x + num}.map{|x|x.chr}.join
+# end
+
+# def easy_to_read_ceasar_cipher(input, shift_number)
+#   letter_array = input.split(//)
+#   number_array = letter_array.map{|letter| letter.ord}
+#   shift_num_array = number_array.map{|num| num + shift_number}
+#   scrambled_string = shift_num_array.map {|shift_num| shift_num.chr}.join
+# end
+# p ceasar_cipher("hello, I'm Alexz", 1)
+
+# p easy_to_read_ceasar_cipher("hello, I'm Alexz", 1)
+
+#so i recognize my first attempt at the ceasar_cipher was kinda weak, so now im going to make the good one, but i should also be able to have the tests work
 
 def ceasar_cipher(input, num)
-  input.split(//).map {|x|x.ord}.map{|x| x + num}.map{|x|x.chr}.join
+  input.split(//).map {|x|x.ord}#.map{|x| x + num}.map{|x|x.chr}.join
 end
 
 def easy_to_read_ceasar_cipher(input, shift_number)
@@ -28,14 +26,31 @@ def easy_to_read_ceasar_cipher(input, shift_number)
   shift_num_array = number_array.map{|num| num + shift_number}
   scrambled_string = shift_num_array.map {|shift_num| shift_num.chr}.join
 end
-p ceasar_cipher("hello, I'm Alexz", 50)
 
-p easy_to_read_ceasar_cipher("hello, I'm Alexz", 50)
+# p ceasar_cipher("hello, I'm Alexz", 1)
 
-#notes below on how to do a more challenging 'more correct' version
+# p ceasar_cipher("AZaz", 1)
 
-# how will i acomadate z to a or Z to A? ASCII values will need to be researched
+# p ceasar_cipher("?>!$*(", 1)
 
-#see further below for less rigrous version, i guess the only test for whether the method is viable is if you can unscramble it, i started to go down that road, but didn't b/c i think i acomplished assignment goals
+# p ceasar_cipher("9", 1)
 
-#so im going not do the more challenging version, but if i did, the mvp migth be to have a cap on the number of ASCII characters you can jump forward (limit the 2nd arg passed in to no more than 5, like a fliter). The next more challenging way to build the cipher is to have the ascii values range and only have the values shift within the range. Idk, i think I acomplished the goal of the project and am moving on while recognizing this could be improved
+# so the real range for asci characters i'm looking for is
+
+# [32 - 126]
+  # AZ[65..90]
+  # az[97-122]
+# so i won't get any
+
+#what is my goal: build a more correct ceasar_cipher
+
+def ceasars_palace(input, num)
+  # convert input to numbers
+  # examine numbers, i need a range to work within for caaps/lower
+  #its starting to become a real pain to work with a different range than what i have, i wouldn't think caps are that important
+  array_of_nums = input.split(//).map {|x|x.ord}
+  #if array of nums is between 65-90 or if array of nums is between 97-122, let them cycle between there, other nums cycle in bigger system? I think not, but this is the begining of a more complex crypotgraphic function which does multi mixing
+end
+
+p ceasars_palace("AZ",1)
+p ceasars_palace("az",1)
---
+++ ceasar_cipher_spec.rb
@@ -0,0 +1,7 @@
+#it handles lower case and upercase
+
+#it can be converted back
+
+#it actually works, aka it actually ciphers stuff
+
+#i wont accept characters with less than 32 chr or 126
